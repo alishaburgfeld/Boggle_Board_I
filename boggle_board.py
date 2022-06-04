957cda5416b10a0bc07f1fd3e9f89144937bd73d
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-06-04 16:10:16 -0400

    fixed it so dice are random, and prints correctly.

--- boggle_board.py
+++ boggle_board.py
@@ -7,18 +7,25 @@ class BoggleBoard:
     ________
     ________""")
     print(self.board)
+    self.boggle_dice=[["AAEEGN"], ["ELRTTY"], ["AOOTTW"], ["ABBJOO"], ["EHRTVW"], ["CIMOTU"], ["DISTTY"], ["EIOSST"], ["DELRVY"], ["ACHOPS"], ["HIMNQU"], ["EEINSU"], ["EEGHNW"], ["AFFKPS"], ["HLNNRZ"],["DEILRX"]]
 
+    
   def shake(self):
-    alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    boardstr=""
-    for i in range(0,17):
-      char=random.randrange(0,25)
-      boardstr+=alpha[char]
-    self.board= f"""    {boardstr[0:4]}
-    {boardstr[4:8]}
-    {boardstr[8:12]}
-    {boardstr[12:16]}"""
-    print(self.board)
+    board_array=[[],[],[],[]]
+    for list in board_array:
+        while len(list) < 4:
+            dice_index=random.randrange(0,15)
+            char_index=random.randrange(0,5) 
+            list.append(self.boggle_dice[dice_index][0][char_index])
+    
+    print_board=board_array.copy()
+
+    for list in print_board:
+        for i, char in enumerate(list):
+            if char=="Q":
+                list[i]="Qu "
+            else: list[i]=f"{char}  "
+        print("".join(list))
 
 
 game_one=BoggleBoard()
